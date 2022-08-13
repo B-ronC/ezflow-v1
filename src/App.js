8c892aa62b4d42dc7c0cc1ad4c003ca3865e96aa
B-ronC <74175266+B-ronC@users.noreply.github.com>
2022-08-13 14:02:17 -0700

    changed app.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './app.css';
+import "./App.css";
 
-import Home from './pages/home/Home';
-import Topbar from './components/topbar/Topbar';
-import Sidebar from './components/sidebar/Sidebar';
-import Teamtasks from './pages/teamtasks/Teamtasks';
-import Teammembers from './pages/teammembers/Teammembers';
-import Teamsettings from './pages/teamsettings/Teamsettings';
+import Home from "./pages/home/Home";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Teamtasks from "./pages/teamtasks/Teamtasks";
+import Teammembers from "./pages/teammembers/Teammembers";
+import Teamsettings from "./pages/teamsettings/Teamsettings";
 
 import { Routes, Route, Link } from "react-router-dom";
 
-import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import awsExports from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
 
@@ -20,16 +20,22 @@ function App({ signOut }) {
   return (
     <>
       <Topbar />
-      <div className='container'>
+      <div className="container">
         <Sidebar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/teamPage/:currTeamID/tasks' element={<Teamtasks />} />
-          <Route path='/teamPage/:currTeamID/members' element={<Teammembers />} />
-          <Route path='/teamPage/:currTeamID/settings' element={<Teamsettings />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/teamPage/:currTeamID/tasks" element={<Teamtasks />} />
+          <Route
+            path="/teamPage/:currTeamID/members"
+            element={<Teammembers />}
+          />
+          <Route
+            path="/teamPage/:currTeamID/settings"
+            element={<Teamsettings />}
+          />
         </Routes>
       </div>
-      <Link to={'/'}>
+      <Link to={"/"}>
         <button onClick={signOut}>Sign out</button>
       </Link>
     </>
